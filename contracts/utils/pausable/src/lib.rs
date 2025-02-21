0283441be48494a7e3b45fbab87368bfcf37b71d
Özgün Özerk <ozgunozerk.elo@gmail.com>
2025-02-21 12:17:54 +0300

    [N-06] Rephrasing Suggestions (#53)
    * rephrase
    
    * alignment
    
    ---------
    
    Co-authored-by: brozorec <9572072+brozorec@users.noreply.github.com>

--- contracts/utils/pausable/src/lib.rs
+++ contracts/utils/pausable/src/lib.rs
@@ -3,9 +3,9 @@
 //! This contract module allows implementing a configurable stop mechanism for
 //! your contract.
 //!
-//! By implementing the trait [`Pausable`] for your contract,
-//! you can safely integrate the Pausable functionality.
-//! The trait [`Pausable`] has the following methods:
+//! By implementing the trait [`Pausable`] for your contract, you can safely
+//! integrate the Pausable functionality. The trait [`Pausable`] has the
+//! following methods:
 //! - [`paused()`]
 //! - [`pause()`]
 //! - [`unpause()`]
@@ -15,8 +15,8 @@
 //! multiple extensions or utilities for your contract, the code will be better
 //! organized.
 //!
-//! We also provide two macros `when_paused` and `when_not_paused`.
-//! These macros act as guards for your functions. For example:
+//! We also provide two macros `when_paused` and `when_not_paused`. These macros
+//! act as guards for your functions. For example:
 //!
 //! ```ignore
 //! #[when_not_paused]
@@ -25,18 +25,18 @@
 //! }
 //! ```
 //!
-//! For a safe pause/unpause implementation, we expose the underlying
-//! functions required for the pausing. These functions work with the Soroban
-//! environment required for the Smart Contracts `e: &Env`, and take advantage
-//! of the storage by storing a flag for the pause mechanism.
+//! For a safe pause/unpause implementation, we expose the underlying functions
+//! required for the pausing. These functions work with the Soroban environment
+//! required for the Smart Contracts `e: &Env`, and take advantage of the
+//! storage by storing a flag for the pause mechanism.
 //!
-//! We expect you to utilize these functions (`storage::*`) for implementing
-//! the methods of the `Pausable` trait, along with your custom business logic
+//! We expect you to utilize these functions (`storage::*`) for implementing the
+//! methods of the `Pausable` trait, along with your custom business logic
 //! (authentication, etc.)
 //!
-//! For god knows why, if you want to opt-out of [`Pausable`] trait, and use
-//! `storage::*` functions directly in your contract, well... you can!
-//! But we encourage the use of [`Pausable`] trait instead, due to:
+//! You can opt-out of [`Pausable`] trait, and use `storage::*` functions
+//! directly in your contract if you want more customizability. But we encourage
+//! the use of [`Pausable`] trait instead, due to the following reasons:
 //! - there is no additional cost
 //! - standardization
 //! - you cannot mistakenly forget one of the methods
